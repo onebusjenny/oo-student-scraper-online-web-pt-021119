f17f62e94d5f79450e6d783a74715263690cdc6a
Jenny Chu <jchudesign0930@gmail.com>
2019-04-14 18:40:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,22 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    attributes.each_pair { |key, value| self.send (("#{key}="), value) }
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
     
+    students_array.each { |student_hash| self.new(student_hash)} 
+    students_array = students_array.new
   end
 
   def add_student_attributes(attributes_hash)
-    
+   attributes_hash.each_pair { |key, value| self.send(("#{key}="), value) }
   end
 
   def self.all
-    
+    @@all
   end
 end
 
